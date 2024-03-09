3b8c55527aff53faf7262bc059cf11b1c701f59e
Muhirwa I. Thierry <mithierry1010@gmail.com>
2024-03-09 08:24:16 +0200

    Created SelectableDisplay Custom control and TimeSpanSlider which will be used in AddShortTaskDialogPage.

---
+++ DeepWork/UserControls/SelectableDisplay.cs
@@ -0,0 +1,95 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Markup;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeepWork.UserControls
+{
+	[ContentProperty(Name ="Content")]
+	public sealed class SelectableDisplay : Control
+	{
+		private Button Display;
+		private static Dictionary<string, List<SelectableDisplay>> _groups = new();
+		public bool IsSelected
+		{
+			get { return (bool)GetValue(IsSelectedProperty); }
+			set {
+				SetValue(IsSelectedProperty, value);
+				UpdateVisualState();
+				if (!string.IsNullOrEmpty(GroupName) && value == true)
+				{
+					foreach (var item in _groups[GroupName])
+					{
+						if (item == this) continue;
+						item.IsSelected = false;
+					}
+				}
+			}
+		}
+		public static readonly DependencyProperty IsSelectedProperty =
+			DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(SelectableDisplay), new PropertyMetadata(false));
+
+		public string GroupName
+		{
+			get { return (string)GetValue(GroupNameProperty); }
+			set {
+				if (_groups.ContainsKey(value))
+					_groups[value].Add(this);
+				else
+					_groups.Add(value, new List<SelectableDisplay> { this });
+				SetValue(GroupNameProperty, value);
+			}
+		}
+		public static readonly DependencyProperty GroupNameProperty =
+			DependencyProperty.Register(nameof(GroupName), typeof(string), typeof(SelectableDisplay), new PropertyMetadata(string.Empty));
+
+		public string Content
+		{
+			get { return (string)GetValue(ContentProperty); }
+			set { SetValue(ContentProperty, value); }
+		}
+		public static readonly DependencyProperty ContentProperty =
+			DependencyProperty.Register(nameof(Content), typeof(string), typeof(SelectableDisplay), new PropertyMetadata(string.Empty));
+
+		public SelectableDisplay()
+		{
+			this.DefaultStyleKey = typeof(SelectableDisplay);
+			IsTabStop = true;
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			if (GetTemplateChild(nameof(Display)) is Button button)
+			{
+				Display = button;
+				Display.Click += Display_Click;
+				Display.KeyDown += Display_KeyDown;
+			}
+		}
+
+		private void Display_KeyDown(object sender, KeyRoutedEventArgs e)
+		{
+			// Todo: Add validation codes here.
+			string input = Utils.KeyboardHelpers.GetCharsFromKeys(e.Key, false, false);
+			char[] chars = Content.ToCharArray();
+			Content = chars.Last() + input;
+		}
+
+		private void Display_Click(object sender, RoutedEventArgs e)
+		{
+			IsSelected = true;
+		}
+
+		private void UpdateVisualState()
+		{
+			bool useTransitions = false;
+			if (IsSelected)
+				VisualStateManager.GoToState(this,"Selected" ,useTransitions);
+			else
+				VisualStateManager.GoToState(this, "Unselected", useTransitions);
+		}
+	}
+}
---
+++ DeepWork/UserControls/TimeSpanSlider.xaml.cs
@@ -0,0 +1,140 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace DeepWork.UserControls;
+
+public sealed partial class TimeSpanSlider : UserControl
+{
+	public TimeSpan TimeSpan
+	{
+		get { return (TimeSpan)GetValue(TimeSpanProperty); }
+		set { SetValue(TimeSpanProperty, value); }
+	}
+	public static readonly DependencyProperty TimeSpanProperty =
+		DependencyProperty.Register(nameof(TimeSpan), typeof(TimeSpan), typeof(TimeSpanSlider), new PropertyMetadata(TimeSpan.Zero));
+
+	public TimeSpanSlider()
+	{
+		this.InitializeComponent();
+	}
+
+	public string ConvertTimeSpanToString(TimeSpan value, string conversionPart)
+	{
+		TimeSpanParts part = (TimeSpanParts)Enum.Parse(typeof(TimeSpanParts), conversionPart);
+		return part switch
+		{
+			TimeSpanParts.Hours => value.Hours.ToString("00"),
+			TimeSpanParts.Minutes => value.Minutes.ToString("00"),
+			TimeSpanParts.Seconds => value.Seconds.ToString("00"),
+			_ => throw new InvalidOperationException("Unknown conversion type.")
+		};
+	}
+
+	public void ConvertHoursBackToTimeSpan(string value) =>
+		TimeSpan = ConvertBackToTimeSpan(value, TimeSpanParts.Hours);
+	public void ConvertMinutesBackToTimeSpan(string value) =>
+		TimeSpan = ConvertBackToTimeSpan(value, TimeSpanParts.Minutes);
+	public void ConvertSecondsBackToTimeSpan(string value) =>
+		TimeSpan = ConvertBackToTimeSpan(value, TimeSpanParts.Seconds);
+
+
+	private TimeSpan ConvertBackToTimeSpan(string value, TimeSpanParts conversionType)
+	{
+		TimeSpan span = TimeSpan;
+		switch (conversionType)
+		{
+			case TimeSpanParts.Hours:
+				span -= TimeSpan.FromHours(span.Hours);
+				span += TimeSpan.FromHours(int.Parse(value));
+				return span;
+
+			case TimeSpanParts.Minutes:
+				span -= TimeSpan.FromMinutes(span.Minutes);
+				span += TimeSpan.FromMinutes(int.Parse(value));
+				return span;
+
+			case TimeSpanParts.Seconds:
+				span -= TimeSpan.FromSeconds(span.Seconds);
+				span += TimeSpan.FromSeconds(int.Parse(value));
+				return span;
+
+			default:
+				throw new InvalidOperationException("The conversion type is unknown.");
+		}
+	}
+
+	private void UpButton_Click(object sender, RoutedEventArgs e)
+	{
+		string Tag = (sender as Button).Tag as string;
+		TimeSpanParts part = (TimeSpanParts)Enum.Parse(typeof(TimeSpanParts), Tag);
+		switch (part)
+		{
+			case TimeSpanParts.Hours:
+				TimeSpan += TimeSpan.FromHours(1);
+				HoursDisplay.IsSelected = true;
+				break;
+
+			case TimeSpanParts.Minutes:
+				TimeSpan += TimeSpan.FromMinutes(1);
+				MinutesDisplay.IsSelected = true;
+				break;
+
+			case TimeSpanParts.Seconds:
+				TimeSpan += TimeSpan.FromSeconds(1);
+				SecondDisplay.IsSelected = true;
+				break;
+
+			default:
+				throw new InvalidOperationException("Unknown button tag.");
+		}
+		DisplayBorder.Focus(FocusState.Programmatic);
+	}
+
+	private void DownButton_Click(object sender, RoutedEventArgs e)
+	{
+		string Tag = (sender as Button).Tag as string;
+		TimeSpanParts part = (TimeSpanParts)Enum.Parse(typeof(TimeSpanParts), Tag);
+		switch (part)
+		{
+			case TimeSpanParts.Hours:
+				TimeSpan -= TimeSpan.FromHours(1);
+				HoursDisplay.IsSelected = true;
+				break;
+
+			case TimeSpanParts.Minutes:
+				TimeSpan -= TimeSpan.FromMinutes(1);
+				MinutesDisplay.IsSelected = true;
+				break;
+
+			case TimeSpanParts.Seconds:
+				TimeSpan -= TimeSpan.FromSeconds(1);
+				SecondDisplay.IsSelected = true;
+				break;
+			default:
+				throw new InvalidOperationException("Unknown button tag.");
+		}
+		DisplayBorder.Focus(FocusState.Programmatic);
+	}
+
+	private void DisplayBorder_GotFocus(object sender, RoutedEventArgs e)
+	{
+		VisualStateManager.GoToState(this, nameof(Focused), false);
+	}
+
+	private void DisplayBorder_LostFocus(object sender, RoutedEventArgs e)
+	{
+		VisualStateManager.GoToState(this, nameof(Unfocused), false);
+		HoursDisplay.IsSelected = false;
+		MinutesDisplay.IsSelected = false;
+		SecondDisplay.IsSelected = false;
+	}
+
+	private enum TimeSpanParts
+	{
+		None,
+		Hours,
+		Minutes,
+		Seconds
+	}
+}
