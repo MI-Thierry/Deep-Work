31a88742ae579ab6c5ff8809d8d2eae191da29f0
Muhirwa I. Thierry <mithierry1010@gmail.com>
2024-03-11 12:23:59 +0200

    Replace AccountCreationPage by SignupPage

---
+++ DeepWork/ViewModels/Pages/SignupViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DeepWork.Services;
+using Microsoft.Windows.AppLifecycle;
+
+namespace DeepWork.ViewModels.Pages;
+
+public partial class SignupViewModel : ObservableObject
+{
+	private readonly AccountManagementService _accountManager;
+
+	[ObservableProperty]
+	private string _firstname;
+
+	[ObservableProperty]
+	private string _lastname;
+
+	[ObservableProperty]
+	private string _password;
+
+	public SignupViewModel()
+	{
+		_accountManager = App.GetService<AccountManagementService>();
+	}
+
+	[RelayCommand]
+	private void Signup()
+	{
+		_accountManager.CreateAccount($"{Firstname} {Lastname}", Password);
+		AppInstance.Restart("Restarting");
+	}
+}
