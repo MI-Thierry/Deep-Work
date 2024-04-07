3c2a6b86a380004a0e32a529356c692c449f4ed3
Muhirwa Thierry <mithierry1010@gmail.com>
2024-04-07 07:43:29 +0200

    Formatted floating point numbers in order to make sure that there isn't more than two digits after decimal point

---
+++ DeepWork/Helpers/DoubleToStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace DeepWork.Helpers
+{
+	public class DoubleToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language) =>
+			$"{(double)value:0.00}";
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			if (double.TryParse(value as string, out double d))
+				return d ;
+			throw new InvalidOperationException("Value have to be a floating point number");
+		}
+	}
+}
