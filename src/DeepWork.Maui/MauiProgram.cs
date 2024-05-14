4a0c2ffcb043bf6f26f5965ba1433452ef246cc4
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-14 20:55:41 +0300

    Add configuration and Infrastructure services

--- src/DeepWork.Maui/MauiProgram.cs
+++ src/DeepWork.Maui/MauiProgram.cs
@@ -1,25 +1,32 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DeepWork.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-namespace DeepWork.Maui
+namespace DeepWork.Maui;
+
+public static class MauiProgram
 {
-	public static class MauiProgram
-	{
-		public static MauiApp CreateMauiApp()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        using var stream = FileSystem.OpenAppPackageFileAsync("appsettings.json").Result;
+        builder.Configuration.AddJsonStream(stream);
+
+        // Add Infrastructure to cross-platform app
+        builder.Services.AddInfrastructureServices(builder.Configuration);
 
 #if DEBUG
-			builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-			return builder.Build();
-		}
-	}
+        return builder.Build();
+    }
 }
