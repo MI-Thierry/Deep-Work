592216d16264a368dea3a68f217b55706c732ddd
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-14 20:54:59 +0300

    Add generic host in winui application and configure configuration manager and logging

--- src/DeepWork.Winui/App.xaml.cs
+++ src/DeepWork.Winui/App.xaml.cs
@@ -1,50 +1,40 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
+﻿using DeepWork.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace DeepWork.Winui;
 
-namespace DeepWork.Winui
+public partial class App : Application
 {
-	/// <summary>
-	/// Provides application-specific behavior to supplement the default Application class.
-	/// </summary>
-	public partial class App : Application
-	{
-		/// <summary>
-		/// Initializes the singleton application object.  This is the first line of authored code
-		/// executed, and as such is the logical equivalent of main() or WinMain().
-		/// </summary>
-		public App()
-		{
-			this.InitializeComponent();
-		}
+    public IHost AppHost { get; private set; }
+    public App()
+    {
+        this.InitializeComponent();
+    }
 
-		/// <summary>
-		/// Invoked when the application is launched.
-		/// </summary>
-		/// <param name="args">Details about the launch request and process.</param>
-		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-		{
-			m_window = new MainWindow();
-			m_window.Activate();
-		}
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        var builder = Host.CreateApplicationBuilder();
 
-		private Window m_window;
-	}
+        builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+        builder.Configuration.AddJsonFile("appsettings.json", false);
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        builder.Services.AddHostedService<ApplicationHost>();
+        builder.Services.AddTransient<MainWindow>();
+
+        // Add Infrastructure to windows-platform app
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+
+        AppHost = builder.Build();
+
+        await AppHost.RunAsync();
+    }
 }
---
+++ src/DeepWork.Winui/ApplicationHost.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeepWork.Winui;
+public class ApplicationHost(MainWindow window, ILogger<ApplicationHost> logger) : IHostedService
+{
+    private readonly Window _window = window;
+    private readonly ILogger<ApplicationHost> _logger = logger;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("The application is starting...");
+        if (!cancellationToken.IsCancellationRequested)
+            _window.Activate();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("The application is stopping...");
+        if (!cancellationToken.IsCancellationRequested)
+            _window.Close();
+        return Task.CompletedTask;
+    }
+}
