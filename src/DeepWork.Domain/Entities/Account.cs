d523ee987d997c07bab45d6e15c8ed32a45dece2
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-15 09:28:30 +0300

    Add Entities in Domain

---
+++ src/DeepWork.Domain/Entities/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace DeepWork.Domain.Entities;
+
+public class Account
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ src/DeepWork.Domain/Entities/ShortTask.cs
@@ -0,0 +1,23 @@
+﻿using DeepWork.Domain.Common;
+using SQLite;
+
+namespace DeepWork.Domain.Entities;
+
+[Table("short-tasks")]
+public class ShortTask : HasDomainEventBase
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(255)]
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    [Indexed]
+    public int LongTaskId { get; set; }
+}
