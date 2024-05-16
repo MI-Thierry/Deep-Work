6ecfc5501adfda11042667235a7e2cbbe5c39565
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-16 06:48:49 +0300

    refactor: add DTOs in infrastructure layer to remove dependency on domain entities
    This adds DTOs which is used in SQLite tables mapping. Now every SQLite table will have its repository which is collected in DeepWorkRepositories
    
    BREAKING CHANGE
    Before this refactor DeepWorkRepository was using domain entities as SQLite table mappings which was adding SQLite dependency in domain layer

---
+++ src/DeepWork.Infrastructure/Common/IDeepWorkRepository.cs
@@ -0,0 +1,8 @@
+﻿using DeepWork.Infrastructure.Data;
+
+namespace DeepWork.Infrastructure.Common;
+public  interface IDeepWorkRepository
+{
+    LongTaskRepository LongTaskRepository { get; }
+    ShortTaskRepository ShortTaskRepository { get; }
+}
--- src/DeepWork.Infrastructure/Common/IRepository.cs
+++ src/DeepWork.Infrastructure/Common/IRepository.cs
@@ -1,3 +1,15 @@
 ﻿namespace DeepWork.Infrastructure.Common;
-public interface IRepository
-{}
+public interface IRepository<T>
+{
+    public Task<T> AddAsync(T task);
+
+    public Task UpdateAsync(T task);
+
+    public Task<T> GetByIdAsync(int id);
+
+    public Task<IEnumerable<T>> GetAllAsync();
+
+    public Task DeleteAsync(T task);
+
+    public Task DeleteAllAsync();
+}
