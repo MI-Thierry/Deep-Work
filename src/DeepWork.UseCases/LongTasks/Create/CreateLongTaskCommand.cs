e4e1f0e2a6fc127a9a9553b139196e22383e41bb
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-16 19:16:59 +0300

    refactor: Change the namespace and name of application layer project

---
+++ src/DeepWork.UseCases/LongTasks/Create/CreateLongTaskCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace DeepWork.UseCases.LongTasks.Create;
+
+public record CreateLongTaskCommand(string Name, DateOnly StartDate, DateOnly EndDate, string? Description) : IRequest;
---
+++ src/DeepWork.UseCases/LongTasks/Create/CreateLongTaskHandle.cs
@@ -0,0 +1,16 @@
+﻿using DeepWork.Domain.Entities;
+using DeepWork.SharedKernel;
+using MediatR;
+
+namespace DeepWork.UseCases.LongTasks.Create;
+public class CreateLongTaskHandle : IRequestHandler<CreateLongTaskCommand>
+{
+    public CreateLongTaskHandle(IRepository<LongTask> _repository)
+    {
+        
+    }
+    public Task Handle(CreateLongTaskCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
