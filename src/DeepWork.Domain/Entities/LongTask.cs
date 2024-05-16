562916d7dca65b5a60f6572a578cabf2e902e56e
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-16 13:29:40 +0300

    feat: Add some business rules in the entities
    This adds rules that governs how to entity values are initialized and updated

--- src/DeepWork.Domain/Entities/LongTask.cs
+++ src/DeepWork.Domain/Entities/LongTask.cs
@@ -1,16 +1,39 @@
-﻿using DeepWork.Domain.Common;
+﻿using Ardalis.GuardClauses;
+using DeepWork.SharedKernel;
 
 namespace DeepWork.Domain.Entities;
 
-public class LongTask : HasDomainEventBase
+public class LongTask(string name, DateOnly startDate, DateOnly endDate, string? description = null) : EntityBase
 {
-    public int Id { get; set; }
+    public const int NameLength = 64;
+    public const int DescriptionLength = 1024;
+    public string Name { get; private set; } = Guard.Against.NullOrEmpty(name);
 
-    public required string Name { get; set; }
+    public string Description { get; private set; } =
+        Guard.Against.StringTooLong(description ?? string.Empty, DescriptionLength);
 
-    public string? Description { get; set; }
+    public DateOnly StartDate { get; private set; } =
+        Guard.Against.Expression(date => date < DateOnly.FromDateTime(DateTime.Now),
+            startDate, "Start date needs to be greater than or equal to today");
 
-    public DateTime StartDate { get; set; }
+    public DateOnly EndDate { get; private set; } =
+        Guard.Against.Expression(date => date < startDate,
+            endDate, "End date needs to be greater than or equal to start date");
 
-    public DateTime EndDate { get; set; }
+    public void UpdateName(string name) => Name = Guard.Against.NullOrEmpty(name);
+
+    public void UpdateStartDate(DateOnly startDate)
+    {
+        StartDate = Guard.Against.Expression(date => date < DateOnly.FromDateTime(DateTime.Now),
+            startDate, "Start date needs to be greater than or equal to today");
+    }
+
+    public void UpdateEndDate(DateOnly endDate)
+    {
+        EndDate = Guard.Against.Expression(date => date < StartDate,
+            endDate, "End date needs to be greater than or equal to start date");
+    }
+
+    public void UpdateDescription(string description) =>
+        Description = Guard.Against.StringTooLong(description, DescriptionLength);
 }
--- src/DeepWork.Domain/Entities/ShortTask.cs
+++ src/DeepWork.Domain/Entities/ShortTask.cs
@@ -1,18 +1,43 @@
-﻿using DeepWork.Domain.Common;
+﻿using Ardalis.GuardClauses;
+using DeepWork.SharedKernel;
 
 namespace DeepWork.Domain.Entities;
 
-public class ShortTask : HasDomainEventBase
+public class ShortTask(string name, DateTime startTime, DateTime endTime, int longTaskId, string? description = null) : EntityBase
 {
-    public int Id { get; set; }
+    private const string _timeMessage = "Start time and End time should in the same day which is not in past";
+    public const int NameLength = 64;
+    public const int DescriptionLength = 256;
+    public string Name { get; private set; } = Guard.Against.NullOrEmpty(name);
 
-    public required string Name { get; set; }
+    public string? Description { get; private set; } =
+        Guard.Against.StringTooLong(description ?? string.Empty, DescriptionLength);
 
-    public string? Description { get; set; }
+    public DateTime StartTime { get; private set; } =
+        Guard.Against.Expression(date => DateOnly.FromDateTime(date) != DateOnly.FromDateTime(endTime)
+        || date < DateTime.Now, startTime, _timeMessage);
 
-    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; private set; } =
+        Guard.Against.Expression(date => DateOnly.FromDateTime(date) != DateOnly.FromDateTime(startTime)
+        || date < startTime, endTime, _timeMessage);
 
-    public DateTime EndTime { get; set; }
+    public int LongTaskId { get; private set; } =
+        Guard.Against.InvalidInput(longTaskId, nameof(longTaskId), id => id > 0);
 
-    public int LongTaskId { get; set; }
+    public void UpdateName(string updateName) => Name = Guard.Against.NullOrEmpty(updateName);
+
+    public void UpdateStartDate(DateTime updateTime)
+    {
+        StartTime = Guard.Against.Expression(time => DateOnly.FromDateTime(time) != DateOnly.FromDateTime(EndTime)
+        || time < DateTime.Now, updateTime, _timeMessage);
+    }
+
+    public void UpdateEndDate(DateTime updateTime)
+    {
+        EndTime = Guard.Against.Expression(time => DateOnly.FromDateTime(time) != DateOnly.FromDateTime(StartTime)
+        || time >= StartTime, updateTime, _timeMessage);
+    }
+
+    public void UpdateDescription(string updateDescription) =>
+        Description = Guard.Against.StringTooLong(updateDescription, DescriptionLength);
 }
