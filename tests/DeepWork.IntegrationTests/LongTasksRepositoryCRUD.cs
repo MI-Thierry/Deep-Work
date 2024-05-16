959ad976642a6e45267a68a2e364c18f5893f586
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-16 09:30:10 +0300

    test: Correct infrastructure integration tests to match with current implementation

---
+++ tests/DeepWork.IntegrationTests/LongTasksRepositoryCRUD.cs
@@ -0,0 +1,94 @@
+﻿using DeepWork.Infrastructure.Models;
+
+namespace DeepWork.IntegrationTests;
+public class LongTasksRepositoryCRUD : BaseDeepWorkRepoTest
+{
+    [Fact]
+    public async void AddsLongTaskAndSetsId()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startDate = DateTime.Now;
+        DateTime endDate = DateTime.Now + TimeSpan.FromDays(1);
+
+        LongTaskDTO longTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartDate = startDate,
+            EndDate = endDate,
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.LongTaskRepository.AddAsync(longTask);
+
+        var newLongTask = (await repo.LongTaskRepository.ListAsync())
+            .FirstOrDefault(task => task.Name == name);
+
+        Assert.Equal(name, newLongTask?.Name);
+        Assert.Equal(description, newLongTask?.Description);
+        Assert.Equal(startDate, newLongTask?.StartDate);
+        Assert.Equal(endDate, newLongTask?.EndDate);
+        Assert.True(newLongTask?.Id > 0);
+    }
+
+    [Fact]
+    public async void UpdatesLongTask()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startDate = DateTime.Now;
+        DateTime endDate = DateTime.Now + TimeSpan.FromDays(1);
+
+        LongTaskDTO longTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartDate = startDate,
+            EndDate = endDate,
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.LongTaskRepository.AddAsync(longTask);
+        var newTask = (await repo.LongTaskRepository.ListAsync())
+            .FirstOrDefault(task => task.Name == name);
+
+        Assert.NotNull(newTask);
+        Assert.NotSame(newTask, longTask);
+
+        var newName = Guid.NewGuid().ToString();
+        newTask.Name = newName;
+        await repo.LongTaskRepository.UpdateAsync(newTask);
+
+        var updatedTask = await repo.LongTaskRepository.GetByIdAsync(newTask.Id);
+
+        Assert.NotNull(updatedTask);
+        Assert.NotEqual(newName, longTask.Name);
+        Assert.Equal(description, updatedTask.Description);
+        Assert.Equal(startDate, updatedTask.StartDate);
+        Assert.Equal(endDate, updatedTask.EndDate);
+    }
+
+    [Fact]
+    public async void DeletesLongTask()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startDate = DateTime.Now;
+        DateTime endDate = DateTime.Now + TimeSpan.FromDays(1);
+
+        LongTaskDTO longTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartDate = startDate,
+            EndDate = endDate,
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.LongTaskRepository.AddAsync(longTask);
+        await repo.LongTaskRepository.DeleteAsync(longTask);
+
+        Assert.DoesNotContain(await repo.LongTaskRepository.ListAsync(), entity => entity.Name == name);
+    }
+}
---
+++ tests/DeepWork.IntegrationTests/ShortTasksRepositoryCRUD.cs
@@ -0,0 +1,100 @@
+﻿using DeepWork.Infrastructure.Models;
+
+namespace DeepWork.IntegrationTests;
+public class ShortTasksRepositoryCRUD : BaseDeepWorkRepoTest
+{
+    [Fact]
+    public async void AddsShortTaskAndSetsId()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
+        int longTaskId = 1;
+
+        ShortTaskDTO shortTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartTime = startTime,
+            EndTime = endTime,
+            LongTaskId = longTaskId
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.ShortTaskRepository.AddAsync(shortTask);
+
+        var newShortTask = (await repo.ShortTaskRepository.ListAsync())
+            .FirstOrDefault(task => task.Name == name);
+
+        Assert.NotNull(newShortTask);
+        Assert.Equal(name, newShortTask.Name);
+        Assert.Equal(description, newShortTask.Description);
+        Assert.Equal(startTime, newShortTask.StartTime);
+        Assert.Equal(endTime, newShortTask.EndTime);
+        Assert.Equal(longTaskId, newShortTask.LongTaskId);
+        Assert.True(newShortTask.Id > 0);
+    }
+
+    [Fact]
+    public async void UpdatesShortTask()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
+        int longTaskId = 1;
+
+        ShortTaskDTO shortTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartTime = startTime,
+            EndTime = endTime,
+            LongTaskId = longTaskId,
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.ShortTaskRepository.AddAsync(shortTask);
+        var newTask = (await repo.ShortTaskRepository.ListAsync())
+            .FirstOrDefault(task => task.Name == name);
+
+        Assert.NotNull(newTask);
+        Assert.NotSame(name, newTask);
+
+        var newName = Guid.NewGuid().ToString();
+        await repo.ShortTaskRepository.UpdateAsync(newTask);
+        var updatedTask = await repo.ShortTaskRepository.GetByIdAsync(newTask.Id);
+
+        Assert.NotNull(updatedTask);
+        Assert.NotEqual(newName, shortTask.Name);
+        Assert.Equal(description, updatedTask.Description);
+        Assert.Equal(startTime, updatedTask.StartTime);
+        Assert.Equal(endTime, updatedTask.EndTime);
+    }
+
+    [Fact]
+    public async void DeletesShortTask()
+    {
+        string name = Guid.NewGuid().ToString();
+        string description = "Test description";
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
+        int longTaskId = 1;
+
+        ShortTaskDTO shortTask = new()
+        {
+            Name = name,
+            Description = description,
+            StartTime = startTime,
+            EndTime = endTime,
+            LongTaskId = longTaskId,
+        };
+
+        var repo = GetDeepWorkRepo();
+        await repo.ShortTaskRepository.AddAsync(shortTask);
+        await repo.ShortTaskRepository.DeleteAsync(shortTask);
+
+        Assert.DoesNotContain(await repo.ShortTaskRepository.ListAsync(), task => task.Name == name);
+    }
+}
