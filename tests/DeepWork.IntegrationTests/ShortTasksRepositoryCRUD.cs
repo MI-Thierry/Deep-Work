69d39ebb9cb69e4e545f319d8863669901b73e40
Muhirwa Thierry <mithierry1010@gmail.com>
2024-05-22 12:33:02 +0200

    test: Correct some things to comply with change in domain rules

--- tests/DeepWork.IntegrationTests/ShortTasksRepositoryCRUD.cs
+++ tests/DeepWork.IntegrationTests/ShortTasksRepositoryCRUD.cs
@@ -1,82 +1,77 @@
 ﻿using DeepWork.Domain.Entities;
+using DeepWork.Domain.Enums;
 using DeepWork.Infrastructure.Data;
 
 namespace DeepWork.IntegrationTests;
 public class ShortTasksRepositoryCRUD : BaseDeepWorkReposTest
 {
-    private readonly ShortTasksRepository _repository;
-    public ShortTasksRepositoryCRUD()
-    {
-        _repository = new ShortTasksRepository(DbPath);
-    }
-    [Fact]
-    public async void AddsShortTaskAndSetsId()
-    {
-        string name = Guid.NewGuid().ToString();
-        string description = "Test description";
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
-        int longTaskId = 1;
+	private readonly ShortTasksRepository _repository;
+	public ShortTasksRepositoryCRUD()
+	{
+		_repository = new ShortTasksRepository(DbPath);
+	}
+	[Fact]
+	public async void AddsShortTaskAndSetsId()
+	{
+		string name = Guid.NewGuid().ToString();
+		string description = "Test description";
+		int longTaskId = 1;
 
-        ShortTask shortTask = new(name, startTime, endTime, longTaskId, description);
+		ShortTask shortTask = new(name, longTaskId, description);
+		shortTask.UpdateTimes(ShortTaskTimeType.StartTime | ShortTaskTimeType.EndTime);
 
-        await _repository.AddAsync(shortTask);
+		await _repository.AddAsync(shortTask);
 
-        var newShortTask = (await _repository.ListAsync())
-            .FirstOrDefault(task => task.Name == name);
+		var newShortTask = (await _repository.ListAsync())
+			.FirstOrDefault(task => task.Name == name);
 
-        Assert.NotNull(newShortTask);
-        Assert.Equal(name, newShortTask.Name);
-        Assert.Equal(description, newShortTask.Description);
-        Assert.Equal(startTime, newShortTask.StartTime);
-        Assert.Equal(endTime, newShortTask.EndTime);
-        Assert.Equal(longTaskId, newShortTask.ParentLongTaskId);
-        Assert.True(newShortTask.Id > 0);
-    }
+		Assert.NotNull(newShortTask);
+		Assert.Equal(name, newShortTask.Name);
+		Assert.Equal(description, newShortTask.Description);
+		Assert.Equal(longTaskId, newShortTask.ParentLongTaskId);
+		Assert.True(newShortTask.Id > 0);
+	}
 
-    [Fact]
-    public async void UpdatesShortTask()
-    {
-        string name = Guid.NewGuid().ToString();
-        string description = "Test description";
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
-        int longTaskId = 1;
+	[Fact]
+	public async void UpdatesShortTask()
+	{
+		string name = Guid.NewGuid().ToString();
+		string description = "Test description";
+		int longTaskId = 1;
 
-        ShortTask shortTask = new(name, startTime, endTime, longTaskId, description);
+		ShortTask shortTask = new(name, longTaskId, description);
+		shortTask.UpdateTimes(ShortTaskTimeType.StartTime | ShortTaskTimeType.EndTime);
 
-        await _repository.AddAsync(shortTask);
-        var newTask = (await _repository.ListAsync())
-            .FirstOrDefault(task => task.Name == name);
+		await _repository.AddAsync(shortTask);
+		var newTask = (await _repository.ListAsync())
+			.FirstOrDefault(task => task.Name == name);
 
-        Assert.NotNull(newTask);
-        Assert.NotSame(name, newTask);
+		Assert.NotNull(newTask);
+		Assert.NotSame(name, newTask);
 
-        var newName = Guid.NewGuid().ToString();
-        await _repository.UpdateAsync(newTask);
-        var updatedTask = await _repository.GetByIdAsync(newTask.Id);
+		var newName = Guid.NewGuid().ToString();
+		await _repository.UpdateAsync(newTask);
+		var updatedTask = await _repository.GetByIdAsync(newTask.Id);
 
-        Assert.NotNull(updatedTask);
-        Assert.NotEqual(newName, shortTask.Name);
-        Assert.Equal(description, updatedTask.Description);
-        Assert.Equal(startTime, updatedTask.StartTime);
-        Assert.Equal(endTime, updatedTask.EndTime);
-    }
+		Assert.NotNull(updatedTask);
+		Assert.NotEqual(newName, shortTask.Name);
+		Assert.Equal(description, updatedTask.Description);
+	}
 
-    [Fact]
-    public async void DeletesShortTask()
-    {
-        string name = Guid.NewGuid().ToString();
-        string description = "Test description";
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = DateTime.Now + TimeSpan.FromHours(1);
-        int longTaskId = 1;
+	[Fact]
+	public async void DeletesShortTask()
+	{
+		string name = Guid.NewGuid().ToString();
+		string description = "Test description";
+		int longTaskId = 1;
 
-        ShortTask shortTask = new(name, startTime, endTime, longTaskId, description);
+		ShortTask shortTask = new(name, longTaskId, description);
+		shortTask.UpdateTimes(ShortTaskTimeType.StartTime | ShortTaskTimeType.EndTime);
 
-        await _repository.AddAsync(shortTask);
-        await _repository.DeleteAsync(shortTask);
 
-        Assert.DoesNotContain(await _repository.ListAsync(), task => task.Name == name);
-    }
+		await _repository.AddAsync(shortTask);
+		await _repository.DeleteAsync(shortTask);
+
+		Assert.DoesNotContain(await _repository.ListAsync(), task => task.Name == name);
+	}
 }
--- tests/DeepWork.UnitTests/Domain/ShortTaskConstructor.cs
+++ tests/DeepWork.UnitTests/Domain/ShortTaskConstructor.cs
@@ -1,4 +1,5 @@
 ﻿using DeepWork.Domain.Entities;
+using DeepWork.Domain.Enums;
 
 namespace DeepWork.UnitTests.Domain;
 
@@ -7,25 +8,21 @@ public class ShortTaskConstructor
     private readonly string _name = Guid.NewGuid().ToString();
     private readonly string _smallDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut tellus eu mauris tincidunt laoreet.\r\n";
     private readonly string _largeDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo orci, varius eget scelerisque et, pulvinar sed sapien. Phasellus eget orci non lorem blandit ornare sed a erat. In a dignissim ante, ac convallis urna. Mauris quis luctus tellus. Mauris viverra nisl eu nunc tincidunt, sed lacinia erat dictum. Vestibulum ac justo vitae eros pretium sodales. Nam egestas libero vitae feugiat placerat. Maecenas luctus congue mi vitae tristique. Cras sit amet auctor odio. Suspendisse eleifend ligula at turpis vehicula porta. Sed tincidunt sapien pellentesque diam malesuada condimentum. Pellentesque libero nisi, vehicula non est eget, laoreet fermentum quam. Etiam vehicula rutrum ligula, id laoreet sem lobortis in. Nullam gravida orci est, non posuere nunc rhoncus a. Aliquam vel tincidunt mauris, et auctor nisi.\r\n\r\nPraesent ultricies quam neque, vitae tempor nisl ultricies ut. Nam lobortis sapien sed arcu pulvinar egestas. Phasellus eu mi nunc. Proin sed libero lorem. Nullam eros nunc, eleifend nec lectus at, cursus laoreet turpis. Sed commodo pharetra ex, at semper nisl hendrerit ut. Aenean et vehicula ligula. Curabitur vitae purus justo. Nullam ex diam, ultricies at eros vitae, ultricies pulvinar neque. Suspendisse at lorem id diam lacinia dictum.\r\n\r\nAenean faucibus nisl vitae rhoncus ullamcorper. Vivamus sollicitudin mi quis scelerisque condimentum. Etiam ultrices aliquet lectus mauris.";
-    private readonly DateTime _startTime = DateTime.Now + TimeSpan.FromMinutes(1);
-    private readonly DateTime _endTime = DateTime.Now + TimeSpan.FromHours(1);
     private readonly int _longTaskId = 1;
 
     [Fact]
-    public void InitializeLongTask()
+    public void InitializeShortTask()
     {
-        ShortTask shortTask = new(_name, _startTime, _endTime, _longTaskId, _smallDesc);
+        ShortTask shortTask = new(_name, _longTaskId, _smallDesc);
+		shortTask.UpdateTimes(ShortTaskTimeType.StartTime | ShortTaskTimeType.EndTime);
 
         Assert.Equal(_name, shortTask.Name);
         Assert.Equal(_smallDesc, shortTask.Description);
-        Assert.Equal(_startTime, shortTask.StartTime);
-        Assert.Equal(_endTime, shortTask.EndTime);
         Assert.Equal(_longTaskId, shortTask.ParentLongTaskId);
+		Assert.NotEqual(DateTime.MinValue, shortTask.StartTime);
+		Assert.NotEqual(DateTime.MinValue, shortTask.EndTime);
 
-        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(string.Empty, _startTime, _endTime, _longTaskId, _smallDesc));
-        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(_name, DateTime.Now - TimeSpan.FromHours(1), _endTime, _longTaskId, _smallDesc));
-        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(_name, _startTime, DateTime.Now - TimeSpan.FromHours(1), _longTaskId, _smallDesc));
-        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(_name, _startTime, _endTime, -3, _smallDesc));
-        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(_name, _startTime, _endTime, _longTaskId, _largeDesc));
+        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(string.Empty, _longTaskId, _smallDesc));
+        Assert.ThrowsAny<ArgumentException>(() => new ShortTask(_name, _longTaskId, _largeDesc));
     }
 }
